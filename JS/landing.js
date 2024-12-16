581cca0d505635543d2000cf3a1695a294c14cb0
SARASWAT223 <sarsiit223@gmail.com>
2024-12-17 00:09:55 +0530

    landing sm

--- JS/landing.js
+++ JS/landing.js
@@ -23,16 +23,26 @@
 
 // Dynamicall changing the image of the water layer
 
-const responsiveImage = document.querySelector("#Layerw");
+const LayerWater = document.querySelector("#Layerw");
+
+const LayerFirst = document.querySelector("#Layer1");
+const LayerSecond = document.querySelector("#Layer2");
+const LayerThird = document.querySelector("#Layer3");
+
+
+
 const timer = document.getElementById('days');
 function updateImageSource() {
     if (window.innerWidth < 500) {
-        responsiveImage.src = "../images/2024_landing/Layerw_sm.svg";
-        timer.style.display = "none";
+        LayerWater.src = "../images/2024_landing/Layerw_sm.svg";
+        // LayerWater.style.border = "1px solid blue";
+        LayerFirst.src = "../images/2024_landing/Layer1_sm.svg";
+        LayerSecond.src = "../images/2024_landing/Layer2_sm.svg";
+        LayerThird.src = "../images/2024_landing/Layer3_sm.svg";
+        
 
     } else {
         responsiveImage.src = "../images/2024_landing/Layerw.svg"; 
-        // timer.style.display = "block";
 
     }
 }
