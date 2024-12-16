19919e88ab1c94bfc24e36f5aa95ec5243c76721
SARASWAT223 <sarsiit223@gmail.com>
2024-12-16 23:14:17 +0530

    responsive mobile

--- JS/landing.js
+++ JS/landing.js
@@ -1,22 +1,39 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     gsap.registerPlugin(ScrollTrigger)
 
-    gsap.set('#landing_page', {
-        clipPath: 'polygon(14% 0%, 80% 0%, 95% 85%, 2% 100%)',
-        borderRadius : '0 0 40% 10%'
-    })
+//     gsap.set('#landing_page', {
+//         clipPath: 'polygon(14% 0%, 80% 0%, 95% 85%, 2% 100%)',
+//         borderRadius : '0 0 40% 10%'
+//     })
 
-    gsap.from('#landing_page', {
-        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-        borderRadius: '0 0 0 0',
-        ease : 'power1.inOut',
-        markers :'true',
-        scrollTrigger : {
-            trigger: '#landing_page',
-            start: 'center center',
-            end: 'bottom center',
-            scrub: 'true'
+//     gsap.from('#landing_page', {
+//         clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+//         borderRadius: '0 0 0 0',
+//         ease : 'power1.inOut',
+//         markers :'true',
+//         scrollTrigger : {
+//             trigger: '#landing_page',
+//             start: 'center center',
+//             end: 'bottom center',
+//             scrub: 'true'
 
-        }
-    })
-    });
+//         }
+//     })
+//     });
+
+// Dynamicall changing the image of the water layer
+
+const responsiveImage = document.querySelector("#Layerw");
+const timer = document.getElementById('days');
+function updateImageSource() {
+    if (window.innerWidth < 500) {
+        responsiveImage.src = "../images/2024_landing/Layerw_sm.svg";
+        timer.style.display = "none";
+
+    } else {
+        responsiveImage.src = "../images/2024_landing/Layerw.svg"; 
+        // timer.style.display = "block";
+
+    }
+}
+updateImageSource()
