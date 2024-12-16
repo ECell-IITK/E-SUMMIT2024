89d1d08878ceec4f6ad392e14b3df4e328fa1682
SARASWAT223 <sarsiit223@gmail.com>
2024-12-17 02:04:47 +0530

    optimizin landing3

--- JS/landing.js
+++ JS/landing.js
@@ -111,14 +111,14 @@ document.addEventListener("DOMContentLoaded", () => {
     function controlVisibility() {
         const isSmallScreen = window.innerWidth ;
         updateLayers(isSmallScreen < 500);
-        if (RegisterL) RegisterL.classList.toggle("d-none", !isSmallScreen);
-        if (RegisterS) RegisterS.classList.toggle("d-none", isSmallScreen);
+        RegisterL.classList.toggle("d-none", !isSmallScreen);
+        RegisterS.classList.toggle("d-none", isSmallScreen);
 
         function updateLayers(isSmallScreen) {
-            if (LayerWater) LayerWater.src = isSmallScreen < 500? "../images/2024_landing/Layerw_sm.svg" : "../images/2024_landing/Layerw.svg";
-            if (LayerFirst) LayerFirst.src = isSmallScreen < 500 ? "../images/2024_landing/Layer1_sm.svg" : "../images/2024_landing/Layer1.svg";
-            if (LayerSecond) LayerSecond.src = isSmallScreen < 500 ? "../images/2024_landing/Layer2_sm.svg" : "../images/2024_landing/Layer2.svg";
-            if (LayerThird) LayerThird.src = isSmallScreen < 500 ? "../images/2024_landing/Layer3_sm.svg" : "../images/2024_landing/Layer3.svg";
+            LayerWater.src = isSmallScreen < 500? "../images/2024_landing/Layerw_sm.svg" : "../images/2024_landing/Layerw.svg";
+            LayerFirst.src = isSmallScreen < 500 ? "../images/2024_landing/Layer1_sm.svg" : "../images/2024_landing/Layer1.svg";
+            LayerSecond.src = isSmallScreen < 500 ? "../images/2024_landing/Layer2_sm.svg" : "../images/2024_landing/Layer2.svg";
+            LayerThird.src = isSmallScreen < 500 ? "../images/2024_landing/Layer3_sm.svg" : "../images/2024_landing/Layer3.svg";
         }
     }
 
