5a7b0dcb262b389f9ed790c93b4f5c3df9018689
SARASWAT223 <sarsiit223@gmail.com>
2024-12-17 18:16:18 +0530

    commit landingsm

--- JS/landing.js
+++ JS/landing.js
@@ -28,20 +28,23 @@ const LayerSecond = document.querySelector("#Layer2");
 const LayerThird = document.querySelector("#Layer3");
 const RegisterL = document.getElementById("Reg_lg");
 const RegisterS = document.getElementById("Reg_sm");
+const Train = document.getElementById("LayerT");
 
 
 function controlVisibility() {
     if (window.innerWidth < 500) {
-        LayerWater.src = "./images/2024_landing/Layerw_sm.svg";
-        LayerFirst.src = "./images/2024_landing/Layer1_sm.svg";
-        LayerSecond.src = "./images/2024_landing/Layer2_sm.svg";
-        LayerThird.src = "./images/2024_landing/Layer3_sm.svg";
-        RegisterL.classList.remove("d-none");
         
+        RegisterL.classList.remove("d-none");
+
         
 
     } else if (window.innerWidth >= 500) {
         RegisterS.classList.remove("d-none");
+        LayerWater.src = "./images/2024_landing/Layerw.svg";
+        LayerFirst.src = "./images/2024_landing/Layer1.svg";
+        LayerSecond.src = "./images/2024_landing/Layer2.svg";
+        LayerThird.src = "./images/2024_landing/Layer3.svg";
+        Train.src = "./images/2024_landing/LayerT.gif";
 
     }
 }
